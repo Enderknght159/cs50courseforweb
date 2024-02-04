269af78b724baa2ff7a8bf65fb4d66531746a031
enderknight159 <enderbrain10@gmail.com>
2024-02-04 10:51:03 +0100

    v2.3

---
+++ 005.py
@@ -0,0 +1,18 @@
+name = "Yaseen"
+print(name[1])
+
+list = ['yaseen', 'Omar', 'Amr', 'Danger']
+
+print(list[2])
+
+tuple = (2,3,4)
+
+print(tuple[1])
+
+list.append("Lolo")
+print(list[4])
+
+set = set(2)
+set.add(1)
+
+print(set)
